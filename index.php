f6a9de4997d60c7a21c5bb7b2d92c12a0043813e
KB <katie@roachhd.com>
2014-11-12 14:11:53 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>LUSH | Gist Styles</title>
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Inconsolata:400|Hind:300">
+  </head>
+
+  <body>
+    <div class="navbar">
+      <div class="container cf">
+        <h1 class="brand">Make your Gists Lush!</h1>
+
+        <p class="credit">Created by <a href="http://twitter.com/mektball">@mektball</a>. Source on <a href="https://github.com/roachhd/lush-gist">GitHub</a>.</p>
+      </div>
+    </div>
+
+    <div class="main loading">
+      <div class="container">
+        <div class="cf">
+          <div class="intro">
+            <p>The Lush Style Roller allows you quickly and easily create your own css styles for GitHub's Gist widgets and embeds. Fill out the settings below and copy the generated CSS into your project for lush Gists.</p>
+          </div>
+
+          <div class="ad">
+            <script async src="#" id="_adpacks_js"></script>
+          </div>
+        </div>
+
+        <div class="app cf">
+          <div class="left-column">
+            <h2 class="title title-settings">Settings</h1>
+
+            <?php
+
+            $json = file_get_contents( 'assets/js/settings.json' );
+            $settings = json_decode( $json )->settings;
+
+            foreach ( $settings as $category ) :
+
+            ?>
+
+              <div class="section">
+                <h3 class="section-title"><?php echo ucwords( key( $category ) ); ?></h3>
+
+                <?php foreach ( reset( $category ) as $setting  ) : ?>
+
+                  <label for="<?php echo reset( $setting )->id; ?>" class="setting-row cf">
+                    <span class="setting-label"><?php echo reset( $setting )->label; ?></span>
+
+                    <input type="text"
+                      id="<?php echo reset( $setting )->id; ?>"
+                      class="setting-input <?php echo reset( $setting )->type; ?>"
+                      value="<?php echo reset( $setting )->value; ?>"
+                      data-setting-category="<?php echo key( $category ); ?>"
+                      data-setting-object="<?php echo key( $setting ); ?>" />
+                  </label>
+
+                <?php endforeach;  ?>
+              </div>
+
+            <?php endforeach; ?>
+
+          </div>
+
+          <div class="right-column">
+            <h2 class="title title-preview">Preview</h1>
+            <script src="https://gist.github.com/roachhd/779fa77e9b90fe945b0c"></script>
+
+            <h2 class="title title-download">Download</h2>
+            <textarea id="download-styles" class="download-styles"></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <style id="theme-styles"></style>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+    <script src="assets/js/iris.min.js"></script>
+    <script src="assets/js/app.js"></script>
+  </body>
+</html>
